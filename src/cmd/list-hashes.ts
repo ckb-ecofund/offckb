2aa3a8318a9f52b7c184f7a5b5ec3bd4d1fb24d1
RetricSu <inneverland2013@gmail.com>
2024-02-24 10:42:26 +0800

    update docker devnet name

--- src/cmd/list-hashes.ts
+++ src/cmd/list-hashes.ts
@@ -3,7 +3,7 @@ import { execSync } from "child_process";
 export function listHashes() {
   try {
     // Assuming the binary is named 'dep-binary' and is in the same directory
-    const result = execSync("target/ckb/ckb list-hashes  -C docker/ckb", {
+    const result = execSync("target/ckb/ckb list-hashes  -C docker/devnet", {
       stdio: "inherit",
     });
     console.log(result);
