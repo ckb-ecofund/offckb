68c816ad06ac4015321dc7ffd5424ac718126e70
RetricSu <inneverland2013@gmail.com>
2024-02-29 10:45:36 +0800

    refactor: use js to replace cp shell cmd (#20)
    * refactor: use js to replace cp shell cmd
    
    * chore: fmt

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as path from 'path';
 
 export function isFolderExists(folderPath: string): boolean {
   try {
@@ -11,5 +12,65 @@ export function isFolderExists(folderPath: string): boolean {
   } catch (error) {
     // If the access or stat fails, or if it's not a directory, return false
     return false;
+  }
+}
+
+export function copyFolderSync(source: string, destination: string) {
+  if (!fs.existsSync(destination)) {
+    fs.mkdirSync(destination);
+  }
+
+  const files = fs.readdirSync(source);
+
+  for (const file of files) {
+    const currentPath = path.join(source, file);
+    const destinationPath = path.join(destination, file);
+
+    if (fs.statSync(currentPath).isDirectory()) {
+      copyFolderSync(currentPath, destinationPath);
+    } else {
+      fs.copyFileSync(currentPath, destinationPath);
+    }
+  }
+}
+
+export async function copyFilesWithExclusion(sourceDir: string, destinationDir: string, excludedFolders: string[]) {
+  try {
+    // Ensure the destination directory exists
+    await fs.promises.mkdir(destinationDir, { recursive: true });
+
+    // Start copying recursively from the source directory
+    await copyRecursive(sourceDir, destinationDir, excludedFolders);
+  } catch (error) {
+    console.error('An error occurred during copying files:', error);
+  }
+}
+
+// Function to recursively copy files and directories
+export async function copyRecursive(source: string, destination: string, excludedFolders: string[]) {
+  // Get a list of all files and directories in the source directory
+  const files = await fs.promises.readdir(source);
+
+  // Iterate through each file or directory
+  for (const file of files) {
+    const sourcePath = path.join(source, file);
+    const destPath = path.join(destination, file);
+
+    // Get the file's stats
+    const stats = await fs.promises.stat(sourcePath);
+
+    // If it's a directory, recursively copy it (unless it's excluded)
+    if (stats.isDirectory()) {
+      if (excludedFolders.includes(file)) {
+        // Skipping directory: ${sourcePath}
+      } else {
+        // Ensure destination directory exists before copying
+        await fs.promises.mkdir(destPath, { recursive: true });
+        await copyRecursive(sourcePath, destPath, excludedFolders);
+      }
+    } else {
+      // Otherwise, copy the file
+      await fs.promises.copyFile(sourcePath, destPath);
+    }
   }
 }
