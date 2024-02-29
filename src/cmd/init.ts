9261d4b66d6f3dfaf34c557129b1fac68b8787e4
RetricSu <inneverland2013@gmail.com>
2024-02-29 15:10:09 +0800

    refactor: rm the update-config-json.ts on the template

--- src/cmd/init.ts
+++ src/cmd/init.ts
@@ -10,10 +10,8 @@ export function init(name: string, template: string) {
   // add some common code files
   const ckbDotTs = path.resolve(dappTemplatePath, 'ckb.ts');
   const configJson = path.resolve(dappTemplatePath, 'config.json');
-  const updateConfigJsonDotTs = path.resolve(dappTemplatePath, 'update-config-json.ts');
   copyFileSync(ckbDotTs, targetPath);
   copyFileSync(configJson, targetPath);
-  copyFileSync(updateConfigJsonDotTs, targetPath);
 
   console.log(`init CKB dapp project: ${targetPath}`);
 }
