194b1847d7425ea596eb805bfc95b733392039b2
RetricSu <inneverland2013@gmail.com>
2024-03-01 17:41:30 +0800

    chore: hide unavailable tutoricial tempaltes

--- src/cfg/select.ts
+++ src/cfg/select.ts
@@ -1,3 +1,4 @@
+import { Separator } from '@inquirer/prompts';
 import select from '@inquirer/select';
 
 export async function selectTemplate() {
@@ -9,10 +10,24 @@ export async function selectTemplate() {
         value: 'transfer',
         description: 'a simple dapp to check CKB balance and transfer CKB from address to address',
       },
+      new Separator(),
       {
-        name: 'Issue Coin With XUDT scripts',
+        name: 'Issue Token With XUDT scripts(coming)',
+        value: 'xudt',
+        description: 'a simple dapp to issue your own token with XUDT scripts(coming)',
+        disabled: true,
+      },
+      {
+        name: 'Issue Token With Max Supply via Omnilock And XUDT scripts(coming)',
         value: 'xudt',
         description: 'a simple dapp to issue your own token with XUDT scripts',
+        disabled: true,
+      },
+      {
+        name: 'Create NFT With Spore scripts(coming)',
+        value: 'xudt',
+        description: 'a simple dapp to issue your own token with XUDT scripts',
+        disabled: true,
       },
     ],
   });
