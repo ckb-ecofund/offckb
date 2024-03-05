67f5320da119dc55601ff0455917738968145e71
RetricSu <inneverland2013@gmail.com>
2024-03-05 23:24:44 +0800

    feat: refactor xudt to support transfer

--- templates/xudt/lib.ts
+++ templates/xudt/lib.ts
@@ -1,73 +1,11 @@
-import { helpers, Address, Script, hd, config, Cell, commons, BI, utils, CellDep } from '@ckb-lumos/lumos';
-import { values, blockchain } from '@ckb-lumos/base';
+import { helpers, hd, config, Cell, commons, BI, utils } from '@ckb-lumos/lumos';
+import { blockchain, HexString } from '@ckb-lumos/base';
 import { indexer, lumosConfig, rpc } from './ckb';
-import { TransactionSkeletonType } from '@ckb-lumos/helpers';
 import { bytes, number } from '@ckb-lumos/codec';
 import { xudtWitnessType } from './scheme';
+import { addCellDep, generateAccountFromPrivateKey } from './util';
 
 config.initializeConfig(lumosConfig);
-
-type Account = {
-  lockScript: Script;
-  address: Address;
-  pubKey: string;
-};
-export const generateAccountFromPrivateKey = (privKey: string): Account => {
-  const pubKey = hd.key.privateToPublic(privKey);
-  const args = hd.key.publicKeyToBlake160(pubKey);
-  const template = lumosConfig.SCRIPTS['SECP256K1_BLAKE160']!;
-  const lockScript = {
-    codeHash: template.CODE_HASH,
-    hashType: template.HASH_TYPE,
-    args: args,
-  };
-  const address = helpers.encodeToAddress(lockScript, { config: lumosConfig });
-  return {
-    lockScript,
-    address,
-    pubKey,
-  };
-};
-
-export const computeLockScriptHashFromPrivateKey = (privkey: string) => {
-  const { lockScript } = generateAccountFromPrivateKey(privkey);
-  return utils.computeScriptHash(lockScript);
-};
-
-export async function capacityOf(address: string): Promise<BI> {
-  const collector = indexer.collector({
-    lock: helpers.parseAddress(address, { config: lumosConfig }),
-  });
-
-  let balance = BI.from(0);
-  for await (const cell of collector.collect()) {
-    balance = balance.add(cell.cellOutput.capacity);
-  }
-
-  return balance;
-}
-
-export function addCellDep(txSkeleton: TransactionSkeletonType, newCellDep: CellDep): TransactionSkeletonType {
-  const cellDep = txSkeleton.get('cellDeps').find((cellDep) => {
-    return (
-      cellDep.depType === newCellDep.depType &&
-      new values.OutPointValue(cellDep.outPoint, { validate: false }).equals(
-        new values.OutPointValue(newCellDep.outPoint, { validate: false }),
-      )
-    );
-  });
-
-  if (!cellDep) {
-    txSkeleton = txSkeleton.update('cellDeps', (cellDeps) => {
-      return cellDeps.push({
-        outPoint: newCellDep.outPoint,
-        depType: newCellDep.depType,
-      });
-    });
-  }
-
-  return txSkeleton;
-}
 
 export async function issueToken(privKey: string, amount: string) {
   const { lockScript } = generateAccountFromPrivateKey(privKey);
@@ -152,14 +90,11 @@ export async function issueToken(privKey: string, amount: string) {
 
   const hash = await rpc.sendTransaction(tx, 'passthrough');
   console.log('The transaction hash is', hash);
-  alert(`The transaction hash is ${hash}`);
+  return { hash, targetOutput };
 }
 
-export async function queryIssuedTokenCells(privKey: string) {
-  const { lockScript } = generateAccountFromPrivateKey(privKey);
+export async function queryIssuedTokenCells(xudtArgs: HexString) {
   const xudtDeps = lumosConfig.SCRIPTS.XUDT;
-
-  const xudtArgs = utils.computeScriptHash(lockScript) + '00000000';
   const typeScript = {
     codeHash: xudtDeps.CODE_HASH,
     hashType: xudtDeps.HASH_TYPE,
@@ -174,9 +109,14 @@ export async function queryIssuedTokenCells(privKey: string) {
   return collected;
 }
 
-export async function  transferTokenToAddress(udtIssuerArgs: string, privKey: string, amount: string, receiverAddress: string) {
-  const { lockScript: senderLockScript } = generateAccountFromPrivateKey(privKey);
-  
+export async function transferTokenToAddress(
+  udtIssuerArgs: string,
+  senderPrivKey: string,
+  amount: string,
+  receiverAddress: string,
+) {
+  const { lockScript: senderLockScript } = generateAccountFromPrivateKey(senderPrivKey);
+
   const receiverLockScript = helpers.parseAddress(receiverAddress);
 
   const xudtDeps = lumosConfig.SCRIPTS.XUDT;
@@ -214,11 +154,11 @@ export async function  transferTokenToAddress(udtIssuerArgs: string, privKey: st
     data: bytes.hexify(number.Uint128LE.pack(amount)),
   };
 
+  const capacity = helpers.minimalCellCapacity(targetOutput);
+  targetOutput.cellOutput.capacity = '0x' + capacity.toString(16);
   // additional 0.001 ckb for tx fee
   // the tx fee could calculated by tx size
   // this is just a simple example
-  const capacity = helpers.minimalCellCapacity(targetOutput);
-  targetOutput.cellOutput.capacity = '0x' + capacity.toString(16);
   const neededCapacity = BI.from(capacity.toString(10)).add(100000);
   let collectedSum = BI.from(0);
   let collectedAmount = BI.from(0);
@@ -228,42 +168,59 @@ export async function  transferTokenToAddress(udtIssuerArgs: string, privKey: st
     collectedSum = collectedSum.add(cell.cellOutput.capacity);
     collectedAmount = collectedAmount.add(number.Uint128LE.unpack(cell.data));
     collected.push(cell);
-    if ( collectedAmount >= BI.from(amount)) break;
+    if (collectedAmount >= BI.from(amount)) break;
   }
 
-  let changedOutputTokenAmount = bytes.hexify(number.Uint128LE.pack(0));
+  let changeOutputTokenAmount = BI.from(0);
   if (collectedAmount.gt(BI.from(amount))) {
-    //throw new Error(`Not enough Token, ${collectedAmount} < ${BI.from(amount)}`);
-    changedOutputTokenAmount = bytes.hexify(number.Uint128LE.pack(collectedAmount.sub(BI.from(amount)).toString(10)))
+    changeOutputTokenAmount = collectedAmount.sub(BI.from(amount));
   }
 
   const changeOutput: Cell = {
     cellOutput: {
       capacity: '0x0',
       lock: senderLockScript,
-      type: typeScript
+      type: typeScript,
     },
-    data: changedOutputTokenAmount,
+    data: bytes.hexify(number.Uint128LE.pack(changeOutputTokenAmount.toString(10))),
   };
 
-  const changedNeededCapacity = BI.from(helpers.minimalCellCapacity(changeOutput));
-  const extraNeededCapacity = collectedSum.lt(neededCapacity) ? neededCapacity.sub(collectedSum).add(changedNeededCapacity) : collectedSum.sub(neededCapacity).add(changedNeededCapacity);
-  if(extraNeededCapacity.gt(0)){
-    let collectedChangedSum = BI.from(0);
-    const collectedChangeCells: Cell[] = [];
+  const changeOutputNeededCapacity = BI.from(helpers.minimalCellCapacity(changeOutput));
+
+  const extraNeededCapacity = collectedSum.lt(neededCapacity)
+    ? neededCapacity.sub(collectedSum).add(changeOutputNeededCapacity)
+    : collectedSum.sub(neededCapacity).add(changeOutputNeededCapacity);
+
+  if (extraNeededCapacity.gt(0)) {
+    let extraCollectedSum = BI.from(0);
+    const extraCollectedCells: Cell[] = [];
     const collector = indexer.collector({ lock: senderLockScript, type: 'empty' });
     for await (const cell of collector.collect()) {
-      collectedChangedSum = collectedChangedSum.add(cell.cellOutput.capacity);
-      collectedChangeCells.push(cell);
-      if (collectedChangedSum >= extraNeededCapacity) break;
+      extraCollectedSum = extraCollectedSum.add(cell.cellOutput.capacity);
+      extraCollectedCells.push(cell);
+      if (extraCollectedSum >= extraNeededCapacity) break;
     }
 
-    if (collectedChangedSum.lt(extraNeededCapacity)) {
-      throw new Error(`Not enough CKB for change, ${collectedChangedSum} < ${extraNeededCapacity}`);
+    if (extraCollectedSum.lt(extraNeededCapacity)) {
+      throw new Error(`Not enough CKB for change, ${extraCollectedSum} < ${extraNeededCapacity}`);
     }
 
-    changeOutput.cellOutput.capacity = extraNeededCapacity.toHexString();
-    txSkeleton = txSkeleton.update('inputs', (inputs) => inputs.push(...collectedChangeCells));
+    txSkeleton = txSkeleton.update('inputs', (inputs) => inputs.push(...extraCollectedCells));
+
+    const change2Capacity = extraCollectedSum.sub(changeOutputNeededCapacity);
+    if (change2Capacity.gt(61000000000)) {
+      changeOutput.cellOutput.capacity = changeOutputNeededCapacity.toHexString();
+      const changeOutput2: Cell = {
+        cellOutput: {
+          capacity: change2Capacity.toHexString(),
+          lock: senderLockScript,
+        },
+        data: '0x',
+      };
+      txSkeleton = txSkeleton.update('outputs', (outputs) => outputs.push(changeOutput2));
+    } else {
+      changeOutput.cellOutput.capacity = extraCollectedSum.toHexString();
+    }
   }
 
   txSkeleton = txSkeleton.update('inputs', (inputs) => inputs.push(...collected));
@@ -271,25 +228,25 @@ export async function  transferTokenToAddress(udtIssuerArgs: string, privKey: st
   /* 65-byte zeros in hex */
   const lockWitness =
     '0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000';
-    
+
   const inputTypeWitness = xudtWitnessType.pack({ extension_data: [] });
   const outputTypeWitness = xudtWitnessType.pack({ extension_data: [] });
-  const witnessArgs = blockchain.WitnessArgs.pack({ lock: lockWitness, inputType: inputTypeWitness, outputType: outputTypeWitness });
+  const witnessArgs = blockchain.WitnessArgs.pack({
+    lock: lockWitness,
+    inputType: inputTypeWitness,
+    outputType: outputTypeWitness,
+  });
   const witness = bytes.hexify(witnessArgs);
   txSkeleton = txSkeleton.update('witnesses', (witnesses) => witnesses.set(0, witness));
 
   // signing
   txSkeleton = commons.common.prepareSigningEntries(txSkeleton);
   const message = txSkeleton.get('signingEntries').get(0)?.message;
-  const Sig = hd.key.signRecoverable(message!, privKey);
+  const Sig = hd.key.signRecoverable(message!, senderPrivKey);
   const tx = helpers.sealTransaction(txSkeleton, [Sig]);
-  console.log(tx);
+  console.log('tx: ', tx);
 
-  const hash = await rpc.sendTransaction(tx, 'passthrough');
-  console.log('The transaction hash is', hash);
-  alert(`The transaction hash is ${hash}`);
-}
-
-export function readTokenAmount(amount: string) {
-  return number.Uint128LE.unpack(amount);
+  const txHash = await rpc.sendTransaction(tx, 'passthrough');
+  console.log('The transaction hash is', txHash);
+  return { txHash, tx };
 }
