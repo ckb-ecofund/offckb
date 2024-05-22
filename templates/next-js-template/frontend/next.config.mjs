203d4a6244ec1b1a89ff463b5618cbd5d71538fa
RetricSu <inneverland2013@gmail.com>
2024-05-22 17:41:54 +0800

    fix: nextjs template webpack for fs lumos hd

--- templates/next-js-template/frontend/next.config.mjs
+++ templates/next-js-template/frontend/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+
+      fs: false, // simple workaround for the lumos.hd.keystore fs not found problem
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
