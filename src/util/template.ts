f6642f7895e0152b31b4d8abc0ec2733b107bded
RetricSu <inneverland2013@gmail.com>
2024-08-12 09:26:06 +0800

    refactor(template): embed option json to fix network issue

--- src/util/template.ts
+++ src/util/template.ts
@@ -1,10 +1,4 @@
-import {
-  dappTemplateGitRepoUserAndName,
-  dappTemplateGitBranch,
-  dappTemplateGitFolder,
-  bareTemplateGitSelectOptionFile,
-} from '../cfg/const';
-import { Request } from './request';
+import templateOpt from '../../templates/template.json';
 
 export interface BareTemplateOption {
   name: string;
@@ -14,13 +8,6 @@ export interface BareTemplateOption {
   author: string;
 }
 
-export async function loadBareTemplateOpts(): Promise<Array<BareTemplateOption>> {
-  const githubUrl = `https://raw.githubusercontent.com/${dappTemplateGitRepoUserAndName}/${dappTemplateGitBranch}/${dappTemplateGitFolder}/${bareTemplateGitSelectOptionFile}`;
-
-  try {
-    const response = await Request.get(githubUrl);
-    return response.data as Array<BareTemplateOption>;
-  } catch (error: unknown) {
-    throw new Error(`Error fetching JSON: ${(error as Error).message}`);
-  }
+export function loadBareTemplateOpts(): Array<BareTemplateOption> {
+  return templateOpt as Array<BareTemplateOption>;
 }
